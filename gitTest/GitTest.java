35599d800010d6b2c2db0e03810cc10256fd7191
Niels Ferguson <ferguson.niels@gmail.com>
2019-01-23 21:22:55 +0100

    Versie 3

--- gitTest/GitTest.java
+++ gitTest/GitTest.java
@@ -4,6 +4,9 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hallo");
+		// Ja hallo, ik was net zo blij met mn error!
+		
+		System.err.println("Delen is vermenigvuldigen");
 		
 	}
 }
